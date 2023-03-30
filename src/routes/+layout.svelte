92f781e7f9b47cbb00cc0052fcb599757d0e5ffa
Giuseppe Turturiello <giuseppe-pio@hotmail.de>
2023-03-30 13:15:47 +0200

    :bulb: Add comments to highlight specific location for imports/customization/logic

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	//#### IMPORT OF STYLESHEETS AND FONTS ####
 	import "@fontsource/martel";
 	import "@fontsource/quattrocento";
 	// Custom Skeleton theme:
@@ -9,6 +10,20 @@
 
 	// Finally, your application's global stylesheet (sometimes labeled 'app.css')
 	import "../app.css";
+
+	//#### IMPORT OF NPM PACKAGES ####
+
+	//#### IMPORT OF SKELETON COMPONENTS ####
+
+	//#### ADAPT SKELETON COMPONENTS ####
+
+	//#### IMPORT OF CUSTOM COMPONENTS ####
+
+	//#### ADAPT CUSTOM COMPONENTS ####
+
+	//#### ADAPT CURRENT COMPONENT/PAGE/LAYOUT ####
+
+	//#### OTHER ####
 </script>
 
 <slot />
