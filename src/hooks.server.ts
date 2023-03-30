8444df1ba1893e19db9a7aa8fb4c8ed3d1dbbff3
Giuseppe Turturiello <giuseppe-pio@hotmail.de>
2023-03-30 23:51:30 +0200

    :passport_control: Use env variables

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,12 +1,12 @@
+import { SHARE_USERNAME, SHARE_PASSWORD } from "$env/static/private";
 import { redirect, type Handle } from "@sveltejs/kit";
 
-export const handle = (async ({ event, resolve }) => {
+export const handle: Handle = (async ({ event, resolve }) => {
 	const shareUsername = event.cookies.get("shareUsername");
 	const sharePassword = event.cookies.get("sharePassword");
 
 	if (event.url.pathname.startsWith("/protected")) {
-		if (shareUsername != "giuseppe" || sharePassword != "giuseppe") {
-			console.log("redirecting");
+		if (shareUsername != SHARE_USERNAME || sharePassword != SHARE_PASSWORD) {
 			throw redirect(303, "/");
 		}
 	}
