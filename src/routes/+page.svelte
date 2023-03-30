39de11451ea451945f226469c62e0e5a3eb9fc36
Giuseppe Turturiello <giuseppe-pio@hotmail.de>
2023-03-30 23:54:01 +0200

    :sparkles: Finish verification page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,19 @@
 	import { backOut } from "svelte/easing";
 	import AnimateOnLoad from "$lib/components/AnimateOnLoad.svelte";
 
+	import type { ModalComponent, ModalSettings } from "@skeletonlabs/skeleton";
+	import ModalShareVerification from "$lib/components/ModalShareVerification.svelte";
+	import { modalStore } from "@skeletonlabs/skeleton";
+
+	function modalShareVerification(): void {
+		const c: ModalComponent = { ref: ModalShareVerification };
+		const d: ModalSettings = {
+			type: "component",
+			component: c,
+		};
+		modalStore.trigger(d);
+	}
+
 	const welcomeLines: string[] = [
 		"YouStruct",
 		"",
@@ -21,63 +34,55 @@
 </script>
 
 <div class="grid min-h-[100dvh] place-items-center">
-	<!-- <AnimateOnLoad> -->
-	<div class="mx-auto mb-5 max-w-4xl text-center">
-		{#each welcomeLines as line, i}
-			{#if i == 0}
-				<div class="text-9xl font-black">
-					<span
-						class="text-shadow m-1 inline-block"
-						in:fly={{
-							y: 100,
-							delay: 250 * i,
-							easing: backOut,
-						}}>{line}</span
-					>
-				</div>
-			{:else if i != welcomeLines.length - 1}
-				<div class="inline-block text-4xl font-black">
-					<span
-						class="text-shadow m-1 inline-block uppercase"
-						in:fly={{
-							y: 100,
-							delay: 250 * i,
-							easing: backOut,
-						}}>{line}</span
-					>
-				</div>
-			{:else}
-				<div class="inline-block text-5xl">
-					<span
-						class="text-shadow m-1 uppercase"
-						in:fly={{
-							y: 100,
-							delay: 250 * i,
+	<AnimateOnLoad>
+		<div class="mx-auto max-w-4xl text-center">
+			{#each welcomeLines as line, i}
+				{#if i == 0}
+					<div class="text-9xl font-black">
+						<span
+							class="m-1 inline-block"
+							in:fly={{
+								y: 100,
+								delay: 250 * i,
+								easing: backOut,
+							}}>{line}</span
+						>
+					</div>
+				{:else if i != welcomeLines.length - 1}
+					<div class="inline-block text-4xl font-black">
+						<span
+							class="m-1 inline-block uppercase"
+							in:fly={{
+								y: 100,
+								delay: 250 * i,
+								easing: backOut,
+							}}>{line}</span
+						>
+					</div>
+				{:else}
+					<div class="inline-block text-5xl">
+						<span
+							class="m-1 uppercase"
+							in:fly={{
+								y: 100,
+								delay: 250 * i,
+								easing: backOut,
+							}}
+						>
+							{line}
+						</span>
+					</div>
+					<button
+						in:fade={{
+							delay: 250 * (i + 1),
+							duration: 1000,
 							easing: backOut,
 						}}
+						class="btn variant-glass-primary mt-4"
+						on:click={modalShareVerification}>Vallah Billah Bro, ich will sehen!</button
 					>
-						{line}
-					</span>
-				</div>
-
-				<form
-					transition:fade={{
-						delay: 250 * (i + 2),
-						duration: 800,
-						easing: backOut,
-					}}
-					class="text-shadow card mx-auto flex max-w-xs flex-col items-start gap-y-4 p-2 uppercase"
-					action="?/verifyForShare"
-					method="POST"
-				>
-					<label for="shareUsername"> Nutzername </label>
-					<input class="w-full" type="text" name="shareUsername" id="shareUsername" />
-					<label for="sharePassword"> Passwort </label>
-					<input class="w-full" type="password" name="sharePassword" id="sharePassword" />
-					<button type="submit" class="btn variant-filled-success"> Okay, let's go. </button>
-				</form>
-			{/if}
-		{/each}
-	</div>
-	<!-- </AnimateOnLoad> -->
+				{/if}
+			{/each}
+		</div>
+	</AnimateOnLoad>
 </div>
