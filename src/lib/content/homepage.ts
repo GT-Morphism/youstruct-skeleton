7afb2e0bd6d62cebf8d73d2d7b2e5b7d7cf72bdf
Giuseppe Turturiello <giuseppe-pio@hotmail.de>
2023-04-01 17:42:57 +0200

    Move content of homepage into own file

---
+++ src/lib/content/homepage.ts
@@ -0,0 +1,8 @@
+export const heroTeaserStrings: string[] = [
+	"Ausbildungsstellen",
+	"Kein langer Bewerbungsprozess",
+	"Praktikas",
+	"Lernmaterialien",
+	"Karrieretipps",
+	"Hilfen für die Praxis",
+];
