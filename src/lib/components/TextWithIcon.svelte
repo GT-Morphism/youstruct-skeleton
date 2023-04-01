447dd6333ea8e7757daf946772636940f47fa46f
Giuseppe Turturiello <giuseppe-pio@hotmail.de>
2023-04-01 17:42:11 +0200

    :sparkles: Add generic component for text with a icon

---
+++ src/lib/components/TextWithIcon.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { ComponentProps } from "svelte";
+	//interface $$Props extends ComponentProps<IconCheckMark> {}
+	import IconCheckMark from "./icons/IconCheckMark.svelte";
+
+	export let Icon = IconCheckMark;
+	export let fillColor: string = "currentColor";
+	export let innerColor: string = "currentColor";
+	export let size: number = 32;
+	export let text: string = "Default Text";
+</script>
+
+<div class="flex items-center gap-x-2">
+	<Icon {fillColor} {size} {innerColor} />
+	<p>{text}</p>
+</div>
