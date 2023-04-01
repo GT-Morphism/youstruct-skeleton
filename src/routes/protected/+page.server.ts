706d643ed350f3002392a3db2812d8fe3c547208
Giuseppe Turturiello <giuseppe-pio@hotmail.de>
2023-04-01 13:59:28 +0200

    :construction: (Form validation) Major Enhancement of zod contact schema

--- src/routes/protected/+page.server.ts
+++ src/routes/protected/+page.server.ts
@@ -19,11 +19,38 @@ const contactSchema = z.object({
 	contactPhoneNumber: z
 		.string({ required_error: "Sie müssen eine gültige Telefonnummer angeben." })
 		.trim()
-		.min(7, { message: "Überprüfen Sie Ihre Telefonnummer; sie scheint zu kurz zu sein." })
-		.max(15, { message: "Überprüfen Sie Ihre Telefonnummer; sie scheint zu lang zu sein." })
+		.refine((phoneString) => isPhoneNumberTooShort(phoneString), {
+			message: "Überprüfen Sie Ihre Telefonnummer; sie scheint zu kurz zu sein.",
+		})
+		.refine((phoneString) => isPhoneNumberTooLong(phoneString), {
+			message: "Überprüfen Sie Ihre Telefonnummer; sie scheint zu lang zu sein.",
+		})
+		.refine((phoneString) => doesPhoneNumberContainOnlyNumbers(phoneString), {
+			message: "Überprüfen Sie Ihre Telefonnummer; sie enthält ungültige Zeichen.",
+		})
 		.optional(),
 	contactMessage: z.string().optional(),
 });
+
+function isPhoneNumberTooShort(phoneString: string): boolean {
+	return getPhoneStringLength(phoneString) >= 7;
+}
+
+function isPhoneNumberTooLong(phoneString: string): boolean {
+	return getPhoneStringLength(phoneString) <= 15;
+}
+
+function getPhoneStringLength(phoneString: string): number {
+	return getRidOfWhiteSpace(phoneString).length;
+}
+
+function getRidOfWhiteSpace(phoneString: string): string {
+	return phoneString.split(" ").join("").split("+").join("");
+}
+
+function doesPhoneNumberContainOnlyNumbers(phoneString: string): boolean {
+	return /^\d+$/.test(getRidOfWhiteSpace(phoneString));
+}
 
 export const actions: Actions = {
 	manageContactForm: async (event) => {
