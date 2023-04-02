e3c138d5a4a4930ae0db1d9ce6da60440e106a26
Giuseppe Turturiello <giuseppe-pio@hotmail.de>
2023-04-02 18:58:29 +0200

    :bento: (Homepage) Add content for accordion

--- src/lib/content/homepage.ts
+++ src/lib/content/homepage.ts
@@ -6,3 +6,29 @@ export const heroTeaserStrings: string[] = [
 	"Karrieretipps",
 	"Hilfen für die Praxis",
 ];
+
+export const accordionContent: accordionContentItem[] = [
+	{
+		iconName: "IconWorker",
+		summary: "Der perfekte Job",
+		content:
+			"Alle Firmen, die mit Youstruct kooperieren, teilen unser Motto: “Make Handwerk great again!”. Faire Löhne, strukturierte Arbeitszeiten und Anerkennung, die Du und Dein Job verdienst.",
+	},
+	{
+		iconName: "IconFutureClock",
+		summary: "Excuse me – Wir haben 2023",
+		content:
+			"Deshalb sind klassische Bewerbungen absolut von gestern. Du erstellst Dir kostenlos ein Profil. Wir schlagen Dir dabei Firmen vor und Du entscheidest, woran Du Interesse hast. Lehn Dich zurück und erhalte Emails von Firmen, die Dich kennenlernen wollen.",
+	},
+	{
+		iconName: "IconExamAPlus",
+		summary: "Prüfungen einfach bestehen",
+		content:
+			"Wir wollen alles dafür tun, dass Du keine Probleme bei Deiner Ausbildung oder Deinem Studium hast. Du bekommst kostenfrei Lernmaterialien, Prüfungen und Lösungen, damit Du als Azubi oder Student alles schaffst. Alle Materialien werden von Ausbildern und Ingenieure für Dich erstellt.",
+	},
+	{
+		iconName: "IconGroup",
+		summary: "Freunde werben = Inhalte freischalten",
+		content: "Schalte für jeden Freund, der sich Deinerwegen anmeldet, exklusive Inhalte frei.",
+	},
+];
