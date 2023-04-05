45de16bba3a93eef937a8a5bdee78d3bb8d87a58
Giuseppe Turturiello <giuseppe-pio@hotmail.de>
2023-04-05 22:04:27 +0200

    :recycle: Add basic two columns component and use it on homepage

---
+++ src/lib/components/BasicTwoColumns.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let ariaLabel: string;
+	export let role: string = "contentinfo";
+</script>
+
+<section class="mb-48 grid grid-cols-2 place-items-center gap-x-8" {role} aria-label={ariaLabel}>
+	<slot name="leftColumn" />
+	<slot name="rightColumn" />
+</section>
--- src/routes/protected/+page.svelte
+++ src/routes/protected/+page.svelte
@@ -4,6 +4,7 @@
 	import AccordionWithImageToggle from "$lib/components/AccordionWithImageToggle.svelte";
 	import HomepageStepper from "$lib/components/HomepageStepper.svelte";
 	import TextWithIcon from "$lib/components/TextWithIcon.svelte";
+	import BasicTwoColumns from "$lib/components/BasicTwoColumns.svelte";
 
 	import {
 		heroTeaserStrings,
@@ -12,38 +13,39 @@
 	} from "$lib/content/homepage";
 </script>
 
-<section
-	class="mb-48 grid grid-cols-2 place-items-center gap-x-8"
-	role="contentinfo"
-	aria-label="YouStruct: Alles, was Du für Deine Zukunft in der Baubranche brauchst."
->
-	<div class="flex max-w-[70ch] flex-col gap-y-8">
-		<h1>
-			Alles, was Du für <span class="gradient-text-color-primary">Deine Zukunft</span> in der Baubranche
-			brauchst.
-		</h1>
-		<p class="!text-xl">
-			Noch nie war das Finden eines Jobs so einfach. Erstelle Dir ein Profil, nutze die Vorteile und
-			<span class="gradient-text-color-success">werde zum Bauhelden</span>.
-		</p>
-		<div class="grid grid-cols-2 grid-rows-3 gap-y-4">
-			{#each heroTeaserStrings as heroTeaserString}
-				<TextWithIcon
-					fillColor="rgb(var(--color-primary-500))"
-					innerColor="rgb(var(--color-surface-900))"
-					size={32}
-					text={heroTeaserString}
-				/>
-			{/each}
+<BasicTwoColumns ariaLabel="YouStruct: Alles, was Du für Deine Zukunft in der Baubranche brauchst.">
+	<svelte:fragment slot="leftColumn">
+		<div class="flex max-w-[70ch] flex-col gap-y-8">
+			<h1>
+				Alles, was Du für <span class="gradient-text-color-primary">Deine Zukunft</span> in der Baubranche
+				brauchst.
+			</h1>
+			<p class="!text-xl">
+				Noch nie war das Finden eines Jobs so einfach. Erstelle Dir ein Profil, nutze die Vorteile
+				und
+				<span class="gradient-text-color-success">werde zum Bauhelden</span>.
+			</p>
+			<div class="grid grid-cols-2 grid-rows-3 gap-y-4">
+				{#each heroTeaserStrings as heroTeaserString}
+					<TextWithIcon
+						fillColor="rgb(var(--color-primary-500))"
+						innerColor="rgb(var(--color-surface-900))"
+						size={32}
+						text={heroTeaserString}
+					/>
+				{/each}
+			</div>
+			<button class="btn variant-ghost-success self-start">Anmelden</button>
 		</div>
-		<button class="btn variant-ghost-success self-start">Anmelden</button>
-	</div>
-	<img
-		class="shadow-sm shadow-primary-500 rounded-container-token"
-		src={abstractBuildingHero}
-		alt="Außenfassade des Wolkenkratzers Tuletornen in Schweden"
-	/>
-</section>
+	</svelte:fragment>
+	<svelte:fragment slot="rightColumn">
+		<img
+			class="shadow-sm shadow-primary-500 rounded-container-token"
+			src={abstractBuildingHero}
+			alt="Außenfassade des Wolkenkratzers Tuletornen in Schweden"
+		/>
+	</svelte:fragment>
+</BasicTwoColumns>
 
 <section class="mb-48" role="contentinfo" aria-label="Eine Plattform. Tausend Möglichkeiten">
 	<h2 class="gradient-text-color-primary mb-20 text-center !text-6xl/normal">
@@ -59,26 +61,26 @@
 	<HomepageStepper {homepageStepperContent} />
 </section>
 
-<section
-	class="mb-48 grid grid-cols-2 place-items-center gap-x-8"
-	role="contentinfo"
-	aria-label="Während Du lernst, suchen wir den perfekten Job für Dich"
->
-	<img
-		class="shadow-sm shadow-primary-500 rounded-container-token"
-		src={focusOnYourself}
-		alt="Außenfassade des Wolkenkratzers Tuletornen in Schweden"
-	/>
-	<div class="flex max-w-[70ch] flex-col gap-y-8">
-		<h2 class="!text-5xl">
-			Während Du lernst, suchen wir den <span class="gradient-text-color-primary"
-				>perfekten Job</span
-			> für Dich.
-		</h2>
-		<p class="!text-xl">
-			Warum Zeit mit Bewerbungsschreiben verschwenden, wenn Du Dich stattdessen auch auf Dich
-			konzentrieren kannst und sich Firmen bei Dir melden?
-		</p>
-		<button class="btn variant-ghost-success self-start">Anmelden</button>
-	</div>
-</section>
+<BasicTwoColumns ariaLabel="Während Du lernst, suchen wir den perfekten Job für Dich">
+	<svelte:fragment slot="leftColumn">
+		<img
+			class="shadow-sm shadow-primary-500 rounded-container-token"
+			src={focusOnYourself}
+			alt="Außenfassade des Wolkenkratzers Tuletornen in Schweden"
+		/>
+	</svelte:fragment>
+	<svelte:fragment slot="rightColumn">
+		<div class="flex max-w-[70ch] flex-col gap-y-8">
+			<h2 class="!text-5xl">
+				Während Du lernst, suchen wir den <span class="gradient-text-color-primary"
+					>perfekten Job</span
+				> für Dich.
+			</h2>
+			<p class="!text-xl">
+				Warum Zeit mit Bewerbungsschreiben verschwenden, wenn Du Dich stattdessen auch auf Dich
+				konzentrieren kannst und sich Firmen bei Dir melden?
+			</p>
+			<button class="btn variant-ghost-success self-start">Anmelden</button>
+		</div>
+	</svelte:fragment>
+</BasicTwoColumns>
