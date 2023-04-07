1ae526f23f69ebc470fa3d95f066e73dbcba8090
Giuseppe Turturiello <giuseppe-pio@hotmail.de>
2023-04-07 14:44:11 +0200

    :lipstick: Add avatar to basic testimonial card

--- src/lib/components/BasicTestimonialCard.svelte
+++ src/lib/components/BasicTestimonialCard.svelte
@@ -10,10 +10,11 @@
 
 <div class="card grid h-full grid-rows-[auto_1fr] place-items-center p-4">
 	<header>
-		<img
+		<Avatar
 			src="https://source.unsplash.com/random/1280x540?skeleton"
-			class="me-auto ms-auto max-w-md shadow-sm shadow-primary-500 rounded-container-token"
 			alt="Post"
+			width="max-w-[12rem]"
+			border="border-2 border-primary-500 hover:border-success-500 transition-colors"
 		/>
 	</header>
 	<div class="flex flex-col p-4">
