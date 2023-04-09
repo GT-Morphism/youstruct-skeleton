ddcd0b3f709521a153dc2fbfb2275cdc5a481ded
Giuseppe Turturiello <giuseppe-pio@hotmail.de>
2023-04-09 14:24:18 +0200

    :recycle: Make margin bottom and column gap dynamic for two columns component

--- src/lib/components/BasicTwoColumns.svelte
+++ src/lib/components/BasicTwoColumns.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
-	export let ariaLabel: string;
+	export let ariaLabel: string = "";
+	export let marginBottom: string = "mb-48";
+	export let columnGap: string = "gap-x-8";
 </script>
 
-<section class="mb-48 grid grid-cols-2 place-items-center gap-x-8" aria-label={ariaLabel}>
+<section
+	class="{marginBottom} grid grid-cols-2 place-items-center {columnGap}"
+	aria-label={ariaLabel}
+>
 	<slot name="leftColumn" />
 	<slot name="rightColumn" />
 </section>
