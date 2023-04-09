bee2d63290bc4922158d5f9ec5705723e5b26bf9
Giuseppe Turturiello <giuseppe-pio@hotmail.de>
2023-04-09 14:25:32 +0200

    :bento: (Business Page) Add content

---
+++ src/lib/content/business-page.ts
@@ -0,0 +1,69 @@
+export const businessPageIntroContent: businessPageIntroItem[] = [
+	{
+		iconID: 1,
+		content: `Wir sind die erste Anlaufstelle, um das <span class="gradient-text-color-success">Personal in Ihrem Bauunternehmen</span> qualitativ aufzustocken und Sie damit krisensicher zu unterstützen. Wir sind mehr als eine stumpfe Personalvermittlung und vereinen Ausbildung und Karriere auf eine noch nie dagewesenen Art und Weise.`,
+		areColumnsReversed: true,
+	},
+	{
+		iconID: 2,
+		content: `Als Deutschlands erstes Bildungs- und Berufsnetzwerk der Baubranche nutzen wir <span class="gradient-text-color-success">modernste Technologien</span>, wie Matching-Prozesse und stellen unseren Nutzern von Ingenieuren verfasstes didaktisches Lern- sowie Praxis-Know-How.`,
+	},
+	{
+		iconID: 3,
+		content: `Bei reibungslosen Abläufen in Planung und Bauausführung sind <span class="gradient-text-color-success">gut ausgebildete Fachkräfte</span> eine Schlüsselrolle. Die ältere Generation geht langsam in Rente, das Fachwissen geht verloren und die Suche nach Mitarbeitern ist im Arbeitsalltag mühsam und erfolglos.`,
+		areColumnsReversed: true,
+	},
+	{
+		iconID: 4,
+		content: `Das ist ab sofort kein Problem mehr, denn dafür sind wir da. Wir schenken unseren tausenden Nutzern die Wertschätzung, die das Personal der Baubranche braucht und formen damit einen Bewerberpool, aus dem wir genau diejenigen Menschen finden, die zu <span class="gradient-text-color-success">Ihren Bedürfnissen und Ihrem Unternehmen passen</span>.`,
+	},
+];
+
+export const whatWeDoContent: string[] = [
+	"Revolutionieren mit Ihnen den Bausektor",
+	"Fördern Karriere und Ausbildung",
+	"Positionieren Ihr Unternehmen komplett neu",
+	"Liefern Ihnen nachhaltig maßgeschneiderte Arbeitskräfte",
+	"Stehen für Fairness und Wertschätzung",
+];
+
+export const recruitmentContent: recruitmentContentItem[] = [
+	{
+		title: `<span class="gradient-text-color-success">Positionierung</span>`,
+		content:
+			"Seien Sie Vorreiter in der Bildung der Baubranche. Positionieren Sie sich und Ihr Unternehmen neu und zeigen Sie der Bauwelt, warum Sie aus der Masse hervorstechen.",
+	},
+	{
+		title: `<span class="gradient-text-color-success">Personalmatching</span>`,
+		content:
+			"Erhalten Sie genau die talentierten Arbeitskräfte, die Sie brauchen. Durch einen eigens entwickelten Algorithmus können Sie aus einem zugeschnittenen Bewerberpool Kontakt zu Ihren Favoriten aufnehmen.",
+	},
+	{
+		title: `<span class="gradient-text-color-success">Vorstellung</span>`,
+		content:
+			"Ihre Bautalente haben Interesse? Prima. Wir organisieren einen Termin zum Vorstellungsgespräch und stehen Ihnen als Beratung bei Bedarf gerne zur Seite.",
+	},
+	{
+		title: `<span class="gradient-text-color-success">Einstellung</span>`,
+		content:
+			"Wir entlasten Sie von der Suche bis zum Vorstellungsgespräch. Der Einstellungsprozess bleibt natürlich Ihnen überlassen.",
+	},
+];
+
+export const faqContent: faqContentItem[] = [
+	{
+		question:
+			"Warum sollten Sie in eine Plattform investieren, wenn Sie auch bei Instagram, Tiktok oder auf Ihrer eigenen Website Stellen ausschreiben können?",
+		answer: `Auf Social-Media-Kanälen ist der Bildungshintegrund der Zielgruppen unbekannt und daher immer unklar, ob Ihre Stellenanzeigen bei <span class="gradient-text-color-success">relevanten Kontakten</span> ankommen.
+		<br/><br/>
+		Die Folgen sind zum Teil sehr hohe Streuverluste und definitv zu vermeidende unnötige Ausgaben. Mit YouStruct erhalten Sie aus unseren Nutzern einen Bewerberpool, der genau Ihren Vorstellungen entspricht.`,
+	},
+	{
+		question: "Wie werden Nutzer auf YouStruct aufmerksam?",
+		answer: "Schul- und Universitätskooperationen, Tiktokcommunity",
+	},
+	{
+		question: "Was ist, wenn Ihr Kandidat einfach nicht zum Kennenlerngespräch erscheint?",
+		answer: `Für diese sehr seltenen Fälle werden Nutzer automatisch von unserem Johnny Sinns zur Rechenschaft gezogen.`,
+	},
+];
